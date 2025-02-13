69443df6efe9a34517032191152a4ee6e73aadeb
Basetta <patrick@pupazzo.org>
2025-02-13 11:07:36 +0100

    Remove conform

--- init.lua
+++ init.lua
@@ -682,7 +682,14 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+        ts_ls = {
+          init_options = {
+            preferences = {
+              -- other preferences...
+              importModuleSpecifierPreference = 'relative',
+            },
+          },
+        },
         --
 
         lua_ls = {
